8a861bc52dbf251fba4e0a2b6e26e05f3fbefde3
XenithMusic <36333004+XenithMusic@users.noreply.github.com>
2021-03-15 21:55:53 -0400

    Update generatestartingweather.py

--- generatestartingweather.py
+++ generatestartingweather.py
@@ -1,42 +1,73 @@
 import time
 
+conf2 = None
+
 def start():
+    global conf2
     start_type = input("Type [M: Manual, R: Random] >> ")
     if start_type == "M":
         conditions = {}
         conditionstemperaturemode = input("Temperature Mode [F: Fahrenheit, C: Celsius] >> ")
-        conditionstemperature = float(input("Temperature (Native mode) >> "))
-        conditionspressure = float(input("Air Pressure (Inches) >> "))
-        conditionscelsius = round(((conditionstemperature*(9/5)) + 32)*100000)/100000
-        savp = 6.11*10*((7.5*conditionstemperature)/(237.3+conditionstemperature))
-        avp = savp/1.9476
-        conditionshumidity = (avp/savp)*100
+        conditionstemperature = float(input("Temperature (Native mode,7PM) >> "))
+        conditionsdew = float(input("Dew Point (Native mode,7PM) >> "))
+        conditionspressure = float(input("Air Pressure (Inches,7PM) >> "))
+        aconditionstemperature = float(input("Temperature (Native mode,7AM) >> "))
+        aconditionsdew = float(input("Dew Point (Native mode,7AM) >> "))
+        aconditionspressure = float(input("Air Pressure (Inches,7AM) >> "))
 
-        confidence = conditionstemperature-20
-        confidence2 = conditionstemperature-40
-        confidence2 = confidence2*5
-        if confidence <= 0:
-            confidence = 0
-        elif confidence2 >= 100:
-            confidence2 = 100
-            confidence = confidence2
-        else:
-            if confidence2 <= 0:
-                print(confidence)
-                print(confidence2)
-                trueconf = (confidence+0)/2
-            else:
-                print(confidence)
-                print(confidence2)
-                trueconf = (confidence+confidence2)/2
 
-        conditionsrainchance = (trueconf/100)*conditionshumidity
+        # AM
+
+
+        if conditionstemperaturemode == "F":
+            conditionscelsius = round(((conditionstemperature*(9/5)) + 32)*100000)/100000
+            conditionsdewsius = round(((conditionsdew*(9/5)) + 32)*100000)/100000
+
+        print(conditionscelsius)
+        print(conditionsdewsius)
+
+        satvp = 6.11*10*((7.5*conditionscelsius)/(237.3+conditionscelsius))
+        actvp = 6.11*10*((7.5*conditionsdewsius)/(237.3+conditionsdewsius))
+        print(satvp)
+        print(actvp)
+        rh = actvp/satvp
+        rh = rh*100
+        amhum = rh-((round(conditionspressure*100)**(0.90))/100)
+
+
+        # PM
+
+
+        if conditionstemperaturemode == "F":
+            conditionscelsius = round(((aconditionstemperature*(9/5)) + 32)*100000)/100000
+            conditionsdewsius = round(((aconditionsdew*(9/5)) + 32)*100000)/100000
+
+        print(conditionscelsius)
+        print(conditionsdewsius)
+
+        satvp = 6.11*10*((7.5*conditionscelsius)/(237.3+conditionscelsius))
+        actvp = 6.11*10*((7.5*conditionsdewsius)/(237.3+conditionsdewsius))
+        print(satvp)
+        print(actvp)
+        rh = actvp/satvp
+        rh = rh*100
+        pmhum = rh-((round(conditionspressure*100)**(0.90))/100)
+
+
+        # FIN
+
+
+        conditionshumidity = (amhum+pmhum)/2
+
+        print("Humidity at 9PM today: " + str(conditionshumidity))
+
 
         if conditionstemperaturemode == "C":
             conditionstemperature = round(((conditionstemperature*(9/5)) + 32)*100000)/100000
             print(conditionstemperature)
 
-        conditions = {"temp":conditionstemperature,"tempmode":conditionstemperaturemode,"humid":conditionshumidity,"rainchance":conditionsrainchance,"rainchanceconf":trueconf}
+        conditions = {"temp":conditionstemperature,"tempmode":conditionstemperaturemode,"cloudcover":conditionshumidity,"humidity":conditionshumidity}
+
         return conditions
 
 print(start())
